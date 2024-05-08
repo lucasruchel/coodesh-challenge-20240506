7a711fcb76ce109dc36ed2d372094c179af99d96
Lucas Vinicius Ruchel <lukasruchel@gmail.com>
2024-05-08 09:43:56 -0300

    feature: infra configurada

---
+++ infra/main.tf
@@ -0,0 +1,65 @@
+
+resource "aws_instance" "coodesh_ec2" {
+    instance_type = "t3.micro"
+    ami = "${var.ami}"
+
+    subnet_id = "${var.subnet_id}"
+
+    tags = {
+      Name = "Coodesh - nginx"
+      Projeto = "TruckPag"
+    }
+
+    vpc_security_group_ids = [ aws_security_group.coodesh_sg.id ]
+    monitoring = true
+
+    key_name = aws_key_pair.coodesh_key_pair.key_name
+}
+
+resource "aws_key_pair" "coodesh_key_pair" {
+  key_name = "coodesh-key"
+  public_key = file("coodesh-key.pub")
+}
+
+
+
+resource "aws_security_group" "coodesh_sg" {
+  description = "Grupo de Seguranca para regras de entrada/saida da instancia"
+  vpc_id = "${var.vpc}"
+
+  name = "coodesh"
+
+  tags = {
+    Name = "sg-coodesh"
+  }
+}
+
+resource "aws_security_group_rule" "sgr_ssh" {
+  security_group_id = aws_security_group.coodesh_sg.id
+  cidr_blocks = [ "0.0.0.0/0" ]
+  description = "Rule to allow ssh on instance"
+  type = "ingress"
+  to_port = "22"
+  protocol = "tcp"
+  from_port = "0"
+}
+
+resource "aws_security_group_rule" "sgr_http" {
+  security_group_id = aws_security_group.coodesh_sg.id
+  cidr_blocks = [ "0.0.0.0/0" ]
+  description = "Rule to allow http on instance"
+  type = "ingress"
+  to_port = "80"
+  protocol = "tcp"
+  from_port = "0"
+}
+
+resource "aws_security_group_rule" "sgr_out" {
+  security_group_id = aws_security_group.coodesh_sg.id
+  cidr_blocks = [ "0.0.0.0/0" ]
+  description = "Rule to allow egress traffic to all internet"
+  type = "egress"
+  protocol = "-1"
+  to_port = "0"
+  from_port = "0"
+}
---
+++ infra/variables.tf
@@ -0,0 +1,19 @@
+variable "subnet_id" {
+  description = "Subnet ID do az us-east-1a"
+  default = "subnet-08d13238f1597432a"
+}
+
+variable "ami" {
+  description = "AMI for lauched instance"
+  default = "ami-0cfa2ad4242c3168d"
+}
+
+variable "region" {
+  description = "Default region"
+  default = "us-east-1"
+}
+
+variable "vpc" {
+  description = "ID of VPC"
+  default = "vpc-0627ffd6e4f4d0265"
+}
