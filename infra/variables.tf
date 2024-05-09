5f065e7519bfee584b1f0f21303b6c47b3002094
Lucas Vinicius Ruchel <lukasruchel@gmail.com>
2024-05-09 11:18:08 -0300

    fix: update pipeline and terraform to permit execution with parameters

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,11 +1,9 @@
 variable "subnet_id" {
-  description = "Subnet ID do az us-east-1a"
-  default = "subnet-08d13238f1597432a"
+  description = "Subnet ID do az"
 }
 
 variable "ami" {
-  description = "AMI for lauched instance"
-  default = "ami-0cfa2ad4242c3168d"
+  description = "AMI for lauched instance"  
 }
 
 variable "region" {
@@ -15,7 +13,7 @@ variable "region" {
 
 variable "vpc" {
   description = "ID of VPC"
-  default = "vpc-0627ffd6e4f4d0265"
+  
 }
 
 variable "projeto" {
